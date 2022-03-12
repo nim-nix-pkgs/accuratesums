f37e3238683e48122f1a97e3fa93c1936a76b500
hugosenari <hugosenari@gmail.com>
2022-03-12 00:07:04 -0300

    chore: re index accuratesums

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Accurate Floating Point Sums and Products.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."accuratesums-master".url = "path:./master";
+  inputs."accuratesums-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."accuratesums-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Accurate Floating Point Sums and Products.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-AccurateSums-master.flake = false;
+  inputs.src-AccurateSums-master.owner = "lbartoletti";
+  inputs.src-AccurateSums-master.ref   = "refs/heads/master";
+  inputs.src-AccurateSums-master.repo  = "accuratesums";
+  inputs.src-AccurateSums-master.type  = "gitlab";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-AccurateSums-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-AccurateSums-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
